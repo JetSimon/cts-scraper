06ddd8ff0fdc80c23aad043041144097ae028c97
Jet Simon <jetjdsimon@gmail.com>
2023-12-04 15:22:41 -0800

    Update cts-scraper.py

--- cts-scraper.py
+++ cts-scraper.py
@@ -3,13 +3,13 @@ import re
 import urllib.request
 from urllib.request import urlretrieve
 from urllib.parse import urlparse
-
+from tqdm import tqdm
 regex = "(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])"
 
 input_folder = "input"
 
-output_folder = "mod_media"
-output_path = f"../../../../../static/{output_folder}/"
+output_folder = "output"
+output_path = f"https://jetsimon.com/cts-media/public/"
 
 errors = []
 
@@ -19,13 +19,13 @@ files = os.listdir(input_folder)
 
 seen = {}
 
-for filename in files:
+for filename in tqdm(files):
 
     count += 1
     if(filename.startswith(".") or filename == "MODLOADERFILE.html"):
         continue
 
-    print(f"SCRAPING MOD ({count}/{len(files)}): {filename}")
+    #print(f"SCRAPING MOD ({count}/{len(files)}): {filename}")
 
     f = open(input_folder + "/" + filename, "r", encoding="utf8")
     contents = f.read()
@@ -47,22 +47,26 @@ for filename in files:
         path = urlparse(url).path
         ext = os.path.splitext(path)[1]
 
-        if(ext == ""):
-            print("Will not download [" + url + "] because no ext")
+        if "discord" not in url.lower():
+            #print(f"{url} is not a discord url so no worries")
             continue
 
-        if(ext in {".html", ".htm", ".mp3", ".mp4", ".wav", ".php", ".m4a"}):
-            print("Will not download [" + url + "] because forbidden extension " + ext)
+        if(ext == ""):
+            #print("Will not download [" + url + "] because no ext")
+            continue
+
+        if(ext in {".html", ".htm", ".php", ".m4a", ".mp3", ".wav"}):
+            #print("Will not download [" + url + "] because forbidden extension " + ext)
             continue
             
         fname = filename.replace(".html", "") + "_" + str(index) + ext
 
         if url in seen:
-            print(f"Already saw url [{url}] so skipping download and replacing with [{seen[url]}]")
+            #print(f"Already saw url [{url}] so skipping download and replacing with [{seen[url]}]")
             contents = contents.replace(url, seen[url])
             continue
 
-        print(f"Trying to download from [{url}] as [{fname}]")
+        #print(f"Trying to download from [{url}] as [{fname}]")
         try:
             opener.retrieve(url, "output/" + fname)
             contents = contents.replace(url, output_path + fname)
